7faceb87291bfa342d6793a0896279332bbd8b31
Liubov Sireneva <lsireneva@gmail.com>
2022-01-15 18:43:23 +0000

    Added details to html and changes for route recipe details

--- server.py
+++ server.py
@@ -2,6 +2,7 @@
 from flask import Flask, render_template, request
 import spoonacular as sp
 import os
+import json
 from pprint import pformat
 import requests
 
@@ -10,6 +11,9 @@ app = Flask(__name__)
 app.secret_key = 'SECRETSECRETSECRET'
 API_KEY = os.environ['SPOONACULAR_KEY']
 
+# Load movie data from JSON file
+#with open("data/recipe_information.json") as f:
+#    recipe_info = json.loads(f.read())
 
 @app.route('/')
 def homepage():
@@ -73,7 +77,7 @@ def get_recipes_advanced():
                 'includeIngredients': includeIngredients,
                 'type': meal_type,
                 'addRecipeInformation': True,
-                'number': 1, 
+                'number': 5, 
                 'apiKey': API_KEY}
 
     response = requests.get(url, params=payload)
@@ -94,6 +98,8 @@ def get_recipes_advanced():
 
 @app.route('/recipe_details/<recipe_id>')
 def get_recipe_details(recipe_id):
+
+    """Do not delete commented code"""
     
     print("******recipe_id=",recipe_id)
 
@@ -106,14 +112,64 @@ def get_recipe_details(recipe_id):
     if response.status_code != 200:
         response.raise_for_status()
     
-    recipe_details = response.json()
+    recipe_info = response.json()
 
-    summary = recipe_details['summary'].replace("</b>", "").replace("<b>", "")
+    # summary = recipe_details['summary'].replace("</b>", "").replace("<b>", "")
+    # test = summary.split('.')
+    # print("Summary", summary)
+    # print("Test", test)
+    print ("+++++Title", recipe_info["title"])
+    #print (recipe_info['nutrition'])
+
+    for item in recipe_info['nutrition']['nutrients']:
+        name, amount, unit = (
+            item["title"], 
+            item["amount"], 
+            item["unit"],)
+        #print ("+++++name",name)
+        #print ("+++++amount",amount)
+        #print ("+++++unit", unit)
+        if name=="Calories":
+            cal_amount = amount
+            print (cal_amount)
+
+        if name=="Fat":
+            fat = str(amount)+" "+unit
+            print (fat)
+
+        if name=="Protein":
+            protein = str(amount)+" "+unit
+            print (protein)
+
+        if name=="Net Carbohydrates":
+            carbs = str(amount)+" "+unit
+            print (carbs)
+    
+    ingredients_list=[]
+
+    for ingredient in recipe_info['nutrition']['ingredients']:
+        #print(ingredient['name'], ingredient['amount'], ingredient['unit'])
+        item=str(ingredient['amount']) + " " + ingredient['unit']+ " " + ingredient['name']      
+        ingredients_list.append(item)
+    
+    #print("ingredients_list:", ingredients_list)
+
+    instructions_list=[]
+    
+    for step in recipe_info['analyzedInstructions'][0]['steps']:
+        #print(ingredient['name'], ingredient['amount'], ingredient['unit'])
+        instructions_list.append(step['step'])
+    
+
+    summary = recipe_info['summary'].replace("</b>", "").replace("<b>", "")
     test = summary.split('.')
-    print("Summary", summary)
-    print("Test", test)
-    return render_template('recipe_details.html', recipe=recipe_details, summary=summary)
+    # print("Summary", summary)
 
+    # for i in test:
+    #     print("i", i)
+
+    #return render_template('recipe_details.html', recipe=recipe_details, summary=summary)
+    return render_template('recipe_details.html', recipe=recipe_info, calories=cal_amount, fat=fat, protein=protein, carbs=carbs, ingredients_list=ingredients_list, summary=summary, instructions_list=instructions_list)
 
 
 if __name__ == '__main__':
